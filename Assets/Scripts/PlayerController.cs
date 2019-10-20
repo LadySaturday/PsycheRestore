614e755be99417e9a42a09277421e7f681b3508a
Nicole Parsons <nicoleparsons2930@gmail.com>
2019-10-19 20:25:01 -0400

    Setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed ;
+    public float rotationSpeed = 200.0f;
+
+    Rigidbody rb;
+    private Vector3 moveDirection = Vector3.zero;
+
+
+    // reference to character's Animator component
+    private Animator anim;
+
+    void Start()
+    {
+   
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightShift) || Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            // IF Shift key is pressed, run
+            movementSpeed = 2;
+            anim.SetFloat("Speed_f", 2);
+        }
+        else 
+        {
+            // ELSE, set speed limit to walk speed
+            movementSpeed = 1;
+            anim.SetFloat("Speed_f", 1);
+
+        }
+
+
+
+
+
+       // if (rb.isGrounded)
+        //    {
+         //       moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+         //       moveDirection = moveDirection * movementSpeed;
+         //   }
+            //Gravity
+            moveDirection.y -= 10f * Time.deltaTime;
+       //     rb.Move(moveDirection * Time.deltaTime);
+
+            transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
+            transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
+
+        
+     
+        
+        
+
+    }
+}
---
+++ Assets/Scripts/playerController1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float PlayerSpeed = 1;
+    // Start is called before the first frame update
+    void Start()  { }
+    // Update is called once per frame
+    void Update()
+    {
+        float mh = Input.GetAxis("Horizontal");
+        float mv= Input.GetAxis("Vertical");
+        if(mh != 0 || mv != 0)
+        {
+            Vector3 movement = new Vector3(mh, 0, mv);
+            Vector3 newPos = transform.position + movement*PlayerSpeed*Time.deltaTime;
+            transform.position = newPos;
+        }
+    }
+}
