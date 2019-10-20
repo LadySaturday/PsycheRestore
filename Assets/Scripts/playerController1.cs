1e22148a339c89c64b815a03a1472435052d2532
Nicole Parsons <nicoleparsons2930@gmail.com>
2019-10-19 21:14:16 -0400

    update playercontroller

--- Assets/Scripts/playerController1.cs
+++ Assets/Scripts/playerController1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class playerController1 : MonoBehaviour
 {
     public float PlayerSpeed = 1;
+    public float rotationSpeed = 200.0f;
+    private Vector3 moveDirection = Vector3.zero;
     // Start is called before the first frame update
     void Start()  { }
     // Update is called once per frame
@@ -17,6 +19,8 @@ public class playerController : MonoBehaviour
             Vector3 movement = new Vector3(mh, 0, mv);
             Vector3 newPos = transform.position + movement*PlayerSpeed*Time.deltaTime;
             transform.position = newPos;
+            moveDirection.y -= 10f * Time.deltaTime;
+            transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
         }
     }
 }
