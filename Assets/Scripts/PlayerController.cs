5a3413f501879d6439058fcbfbc2569cff9550b1
Nicole Parsons <nicoleparsons2930@gmail.com>
2019-10-20 12:17:49 -0400

    setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movementSpeed ;
+    public static float movementSpeed ;
     public float rotationSpeed = 200.0f;
 
     Rigidbody rb;
@@ -22,40 +22,35 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightShift) || Input.GetKeyDown(KeyCode.LeftShift))
+        updateAnim();
+
+
+            moveDirection.y -= 10f * Time.deltaTime;
+            transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
+            transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);   
+
+    }
+
+
+    void updateAnim()
+    {
+        if ((Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.RightShift)) || (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift)))
         {
-            // IF Shift key is pressed, run
-            movementSpeed = 2;
+            // IF Shift key is pressed while walking, run
+            //movementSpeed = 5;
             anim.SetFloat("Speed_f", 2);
         }
-        else 
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S))
         {
             // ELSE, set speed limit to walk speed
             movementSpeed = 1;
             anim.SetFloat("Speed_f", 1);
+        }
+        else
+        {
+            //ELSE idle
+            anim.SetFloat("Speed_f", 0);
 
         }
-
-
-
-
-
-       // if (rb.isGrounded)
-        //    {
-         //       moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-         //       moveDirection = moveDirection * movementSpeed;
-         //   }
-            //Gravity
-            moveDirection.y -= 10f * Time.deltaTime;
-       //     rb.Move(moveDirection * Time.deltaTime);
-
-            transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
-            transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
-
-        
-     
-        
-        
-
     }
 }
