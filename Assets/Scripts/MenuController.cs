fc462ed3984736602b782614909f149a092affd0
NH <n.hong.8978@gmail.com>
2019-11-07 14:11:55 -0500

    Created new StartMenu, DeathScene and WinScene

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Initializing variables
+    private bool soundIsOn = false;
+    private bool musicIsOn = false;
+    private float soundVolume = 0;
+
+    public Toggle soundOnOff;
+    public Toggle musicOnOff;
+    public Slider soundVolumeSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundOnOff.isOn = soundIsOn;
+        musicOnOff.isOn = musicIsOn;
+        soundVolumeSlider.value = soundVolume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Enter level name through Unity
+    public void LOAD_LEVEL(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void TOGGLE_SOUND()
+    {
+        soundIsOn = !soundIsOn;
+        Debug.Log("soundsIsOn: " + soundOnOff.isOn);
+    }
+
+    public void TOGGLE_MUSIC(bool musicON)
+    {
+        musicIsOn = !musicIsOn;
+        Debug.Log("musicIsOn: " + musicOnOff.isOn);
+        UpdateUI();
+    }
+
+    public void CHANGE_VOLUME(float newValue)
+    {
+        soundVolumeSlider.value = newValue;
+        Debug.Log("soundVolume: " + soundVolume);
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        soundOnOff.isOn = soundIsOn;
+        musicOnOff.isOn = musicIsOn;
+        soundVolumeSlider.value = soundVolume;
+    }
+
+}
