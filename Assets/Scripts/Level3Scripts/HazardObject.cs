2f9e54c5be5f3d04ec3ba60da6e343e346b1916f
klwaamzhang <36008792+klwaamzhang@users.noreply.github.com>
2019-11-30 18:17:51 -0500

    MultiLevel 3 added

--- Assets/Scripts/Level3Scripts/HazardObject.cs
+++ Assets/Scripts/Level3Scripts/HazardObject.cs
@@ -2,19 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HazardObject : MonoBehaviour
 {
     // Yizhi 11/30/2019
+    public Text txtExtinguishTimes;
+    public GameObject DotDiedMessage;
     Transform secondaryPlayer;
     float extinguishDistance = 2;
+    static int extinguishTimes = 10;
+    static byte g = 255;
+    static byte b = 255;
 
     // Start is called before the first frame update
     void Start()
     {
         // Yizhi 11/30/2019
+        txtExtinguishTimes.text = extinguishTimes.ToString();
         //if (MenuController.type.ToString().Equals("Multiplayer"))
-            secondaryPlayer = GameObject.FindGameObjectWithTag("SecondaryPlayer").transform;
+        secondaryPlayer = GameObject.FindGameObjectWithTag("SecondaryPlayer").transform;
     }
 
     // Update is called once per frame
@@ -23,13 +30,25 @@ public class HazardObject : MonoBehaviour
         // Yizhi 11/30/2019
         //if (MenuController.type.ToString().Equals("Multiplayer"))
         //{
-            float d2P = Vector3.Distance(transform.position, secondaryPlayer.position);
-            Debug.Log(d2P);
-            if (d2P <= extinguishDistance)
+        float d2P = Vector3.Distance(transform.position, secondaryPlayer.position);
+        if (d2P <= extinguishDistance)
+        {
+            extinguishTimes -= 1;
+            txtExtinguishTimes.text = extinguishTimes.ToString();
+            g -= 25;
+            b -= 25;
+            Debug.Log("g: " + g + " | b: " + b);
+            GameObject.FindGameObjectWithTag("SecondaryPlayer").GetComponent<Renderer>().material.SetColor("_EmissionColor", new Color32(255, g, b, 255));
+            var main = secondaryPlayer.GetChild(0).GetComponent<ParticleSystem>().main;
+            Color c = new Color32(255, g, b, 255);
+            main.startColor = c;
+            if(extinguishTimes == 0)
             {
-                Debug.Log("d2P <= extinguishDistance");
-                gameObject.active = false;
+                DotDiedMessage.SetActive(true);
+                Destroy(secondaryPlayer.gameObject);
             }
+            gameObject.active = false;
+        }
         //}
     }
 
